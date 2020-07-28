21134116416ecdb0de5a0e0888264ccc9ec0e38b
unknown <anujshashimal456@gmail.com>
2020-07-29 01:42:35 +0530

    first commit

---
+++ TimeTableManagement/TimeTableManagement/DBConnection/DbConn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTableManagement.DBConnection
+{
+    class DbConn
+    {
+        public SqlConnection getDBConnection()
+        {
+            string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Aux-138\source\repos\TimeTableManagement\TimeTableManagement\myDB.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection conn = new SqlConnection(conString);
+            return conn;
+        }
+
+    }
+}
