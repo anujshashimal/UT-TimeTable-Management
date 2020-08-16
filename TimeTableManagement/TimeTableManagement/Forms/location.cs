8af04266b1eec37ddbfe00d7783977da0bedeee1
Aux-138 <anujshashimal456@gmail.com>
2020-08-17 00:29:13 +0530

    location function updated

--- TimeTableManagement/TimeTableManagement/Forms/location.cs
+++ TimeTableManagement/TimeTableManagement/Forms/location.cs
@@ -21,7 +21,7 @@ namespace TimeTableManagement.Forms
 
         public location()
         {
-        InitializeComponent();
+            InitializeComponent();
             con = DBConnection.getDBConnection();
 
         }
@@ -72,7 +72,7 @@ namespace TimeTableManagement.Forms
 
         private void locBtn2_Click(object sender, EventArgs e)
         {
-            
+
 
             locationModel.locationPID = locationID.Text;
             locationModel.locationPName = buildingName.Text;
@@ -97,7 +97,14 @@ namespace TimeTableManagement.Forms
 
         private void locationdataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            var currentRow = locationdataGridView.CurrentRow;
 
+            var selectedName = currentRow.Cells[0].Value;
+            var selectedName1 = currentRow.Cells[1].Value;
+
+
+            locationID.Text = selectedName.ToString();
+            buildingName.Text = selectedName1.ToString();
         }
 
         private void addB_Click(object sender, EventArgs e)
@@ -143,12 +150,31 @@ namespace TimeTableManagement.Forms
 
         private void deleteB1_Click(object sender, EventArgs e)
         {
+            String academicyrsemshldupdatevalue = locationID.Text;
+            SqlDataReader dr = LocationConn.loadLocavalues();
+            if (con.State.ToString() != "Open")
+            {
+                con.Open();
+            }
+            while (dr.Read())
+            {
+                String buildingNme = Convert.ToString(dr["locationID"]);
+                if (academicyrsemshldupdatevalue.Equals(buildingNme))
+                {
+                    String locationID1 = locationID.Text;
+                    string query = "DELETE  FROM LocationTimeTable  WHERE  locationID='" + locationID1 + "'";
+                    SqlCommand com = new SqlCommand(query, con);
+                    com.ExecuteNonQuery();
+                }
+            }
 
+            dr.Close();
+            roomDatagridView.DataSource = LocationConn.GetLocationvalues();
         }
 
         private void roomDatagridView_DoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-       
+
             var currentRow = roomDatagridView.CurrentRow;
 
             var selectedName = currentRow.Cells[0].Value;
@@ -163,6 +189,95 @@ namespace TimeTableManagement.Forms
             roomCapacity.Text = selectedName3.ToString();
             roomCapacityT.Text = selectedName4.ToString();
 
+        }
+
+        private void locationID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            String academicyrsemshldupdatevalue = buildingNameT.Text;
+            String nme1 = roomIDT.Text;
+            String nme2 = roomNameT.Text;
+            String nme3 = roomCapacity.Text;
+            String nme4 = roomCapacityT.Text;
+
+            SqlDataReader dr = LocationConn.loadRoomvalues();
+            if (con.State.ToString() != "Open")
+            {
+                con.Open();
+            }
+            while (dr.Read())
+            {
+                String buildingNme = Convert.ToString(dr["buildingName"]);
+                if (academicyrsemshldupdatevalue.Equals(buildingNme))
+                {
+                    string sql = "UPDATE RoomTable SET roomID='" + nme1 + "' WHERE buildingName = '" + academicyrsemshldupdatevalue + "'";
+                    string sql1 = "UPDATE RoomTable SET roomName='" + nme2 + "' WHERE buildingName = '" + academicyrsemshldupdatevalue + "'";
+                    string sql2 = "UPDATE RoomTable SET roomCapacity='" + nme3 + "' WHERE buildingName = '" + academicyrsemshldupdatevalue + "'";
+                    string sql3 = "UPDATE RoomTable SET roomType='" + nme4 + "' WHERE buildingName = '" + academicyrsemshldupdatevalue + "'";
+
+                    SqlCommand com = new SqlCommand(sql, con);
+                    SqlCommand com2 = new SqlCommand(sql1, con);
+                    SqlCommand com3 = new SqlCommand(sql2, con);
+                    SqlCommand com4 = new SqlCommand(sql3, con);
+
+                    MessageBox.Show("Updated!");
+
+                    com.ExecuteNonQuery();
+                    com2.ExecuteNonQuery();
+                    com3.ExecuteNonQuery();
+                    com4.ExecuteNonQuery();
+
+                }
+            }
+
+            dr.Close();
+            roomDatagridView.DataSource = LocationConn.GetRoomvalues();
+        }
+    
+
+
+        private void updateB1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updateB1_Click_1(object sender, EventArgs e)
+        {
+            String academicyrsemshldupdatevalue = locationID.Text;
+            String nme1 = locationID.Text;
+            String nme2 = buildingName.Text;
+
+            SqlDataReader dr = LocationConn.loadLocavalues();
+            if (con.State.ToString() != "Open")
+            {
+                con.Open();
+            }
+            while (dr.Read())
+            {
+                String locationID = Convert.ToString(dr["locationID"]);
+                if (academicyrsemshldupdatevalue.Equals(locationID))
+                {
+                    string sql = "UPDATE LocationTimeTable SET locationID='" + nme1 + "' WHERE locationID = '" + academicyrsemshldupdatevalue + "'";
+                    string sql1 = "UPDATE LocationTimeTable SET locationName='" + nme2 + "' WHERE locationID = '" + academicyrsemshldupdatevalue + "'";
+
+
+                    SqlCommand com = new SqlCommand(sql, con);
+                    SqlCommand com2 = new SqlCommand(sql1, con);
+
+                    MessageBox.Show("Updated!");
+
+                    com.ExecuteNonQuery();
+                    com2.ExecuteNonQuery();
+
+                }
+            }
+
+            dr.Close();
+            locationdataGridView.DataSource = LocationConn.GetLocationvalues();
         }
     }
 }
