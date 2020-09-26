03465bdc358bad62333d58841d130aa275fd5a77
lahirulakruwan <lahirulakruwan5@gmail.com>
2020-09-26 12:58:35 +0530

    updated manage sessions

--- TimeTableManagement/TimeTableManagement/Forms/Managesessions.designer.cs
+++ TimeTableManagement/TimeTableManagement/Forms/Managesessions.designer.cs
@@ -110,10 +110,10 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabControl1.Location = new System.Drawing.Point(-2, -2);
+            this.tabControl1.Location = new System.Drawing.Point(-2, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1084, 753);
+            this.tabControl1.Size = new System.Drawing.Size(1084, 739);
             this.tabControl1.TabIndex = 0;
             this.tabControl1.Click += new System.EventHandler(this.tabControl1_Click);
             // 
@@ -140,7 +140,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1076, 720);
+            this.tabPage1.Size = new System.Drawing.Size(1076, 706);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Not Avaliable timeslots";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -376,7 +376,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1076, 720);
+            this.tabPage2.Size = new System.Drawing.Size(1076, 706);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Consecutive Sessions";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -612,7 +612,7 @@
             this.tabPage3.Controls.Add(this.label20);
             this.tabPage3.Location = new System.Drawing.Point(4, 29);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1076, 720);
+            this.tabPage3.Size = new System.Drawing.Size(1076, 706);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Parallel Session";
             this.tabPage3.UseVisualStyleBackColor = true;
